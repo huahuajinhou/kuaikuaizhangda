b85549732b21afeb997a1c2566575e3627e123c9
mawk <mawk@emrubik.com>
2017-08-09 17:43:56 +0800

    mk cmt

---
+++ gaofangbaisi/gaofangbaisi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gaofangbaisi
+//
+//  Created by user on 9/8/17.
+//  Copyright © 2017年 srkj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
